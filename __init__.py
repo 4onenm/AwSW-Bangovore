230854f29e4dea384d829cf02ebf5f6f53e2b7f1
4onenm <bluerdigemax@gmail.com>
2022-08-28 08:44:39 -0500

    Remove Anna4 and all hard vore references.

--- __init__.py
+++ __init__.py
@@ -2,31 +2,6 @@ from modloader.modclass import Mod, loadable_mod
 import jz_magmalink as ml
 from bangok_four import register_fetish
 
-def bangovore_four_anna4():
-    ( ml.find_label('a4romance')
-        .search_menu("If you say so.")
-        .branch()
-        .search_say("Not like that! You'll ruin them. I'll do it.")
-        .hook_to('bangovore_four_anna4', return_link=True, condition='persistent.nsfwtoggle == True and annagoodending == False and persistent.bangovore_hardvore == True')
-    )
-
-    ( ml.find_label('bangok_anon_anna4_start')
-        .search_if('bangok_four_anna2.unplayed == True')
-        .link_from('bangovore_four_anna4_bangok_sex')
-    )
-
-    ( ml.find_label('annagoodending')
-        .search_python('_game_menu_screen = None', depth=300)
-        .link_from('bangovore_four_anna4_bangok_epilogue_end')
-    )
-
-    # Paginate Anna nom menus
-    m1 = ( ml.find_label('bangovore_four_anna4_chow_down_menu1_pagination'))
-    ml.ast_link.utils._create_hook(node_from=m1.node, func=ml.pages._show_charmenu, tag="bangovore_four_anna4_chow_down_menu1_paginator") 
-    m2 = ( ml.find_label('bangovore_four_anna4_chow_down_menu2_pagination'))
-    ml.ast_link.utils._create_hook(node_from=m2.node, func=ml.pages._show_charmenu, tag="bangovore_four_anna4_chow_down_menu2_paginator") 
-
-
 def bangovore_four_bryce3():
     ( ml.find_label('bangok_four_bryce3_mcbottom_train_epilogue')
         .search_say("I hope not. Came up with a couple ideas while packing the trash.")
@@ -49,7 +24,6 @@ def bangovore_four_xsebastian():
 
 
 def link_scenes():
-    bangovore_four_anna4()
     bangovore_four_bryce3()
     bangovore_four_xsebastian()
 
@@ -66,17 +40,6 @@ class BangOVoreMod(Mod):
         register_fetish("Soft Vore", 'bangovore_softvore',
             active_image="image/ui/bangovore/icons/bangovore_softvore_active.png",
             inactive_image="image/ui/bangovore/icons/bangovore_softvore_inactive.png",
-        )
-
-        register_fetish("Digestion", 'bangovore_digestion',
-            clean_label="Archeology",
-            image_clean=True,
-            image="image/ui/bangovore/icons/bangovore_digestion.png",
-        )
-
-        register_fetish("Hard Vore", 'bangovore_hardvore',
-            active_image="image/ui/bangovore/icons/bangovore_hardvore_active.png",
-            inactive_image="image/ui/bangovore/icons/bangovore_hardvore_inactive.png",
         )
 
         link_scenes()
